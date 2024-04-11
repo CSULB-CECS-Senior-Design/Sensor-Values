017d66afe896bde180bfb482b37c882849e2260c
mushie4282 <tranmichelle0826@yahoo.com>
2024-04-10 21:54:35 -0600

    added OVOS for speaking aspect

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ This repository contains code for reading data from an MQ-2 gas sensor using Ada
 - Product: https://www.adafruit.com/product/1083
 - Tutorial: https://learn.adafruit.com/adafruit-4-channel-adc-breakouts/python-circuitpython
 - ADS1015 Datasheet: https://cdn-shop.adafruit.com/datasheets/ads1015.pdf
+- Sensor: https://www.haoyuelectronics.com/Attachment/MQ-2/MQ-2.pdf
--- readSensor.py
+++ readSensor.py
@@ -7,6 +7,17 @@ import board
 import busio
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+import logging
+from ovos_bus_client import MessageBusClient, Message
+
+# Setting up client to connect to a local mycroft instance
+client = MessageBusClient()
+client.run_in_thread()
+# Sending speak message
+client.emit(Message('speak', data={'utterance': 'Reading MQ-2 Sensor'}))
+
+#Create log file
+logging.basicConfig(filename='MQ-2.log', format='%(asctime)s: %(levelname)s: %(message)s', level=logging.INFO)
 
 # Create the I2C bus 
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -17,9 +28,12 @@ ads = ADS.ADS1015(i2c)
 #Create single-ended input on channel 0 (Analog Pin 0)
 channelA0 = AnalogIn(ads, ADS.P0)
 
-print("MQ2 Warming Up!")
+logging.info("MQ2 Warming Up!")
 time.sleep(5) # Give 5 seconds to warm up
 
 while True:
-	print(f"raw value: {channelA0.value}     voltage: {channelA0.voltage:.3f} volts")
+	formatted_output = f"raw value: {channelA0.value}     voltage: {channelA0.voltage:.3f} volts"
+	# print(formatted_output)
 	time.sleep(0.5) # allow delay between every read of about .5 sec
+	logging.info(formatted_output)
+	client.emit(Message('speak', data={'utterance': str(channelA0.value)}))
