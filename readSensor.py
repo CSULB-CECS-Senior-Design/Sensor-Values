0d1f3f9a883aaf9595c84b4a92d59fd8eeaca4be
mushie4282 <tranmichelle0826@yahoo.com>
2024-04-10 20:53:44 -0600

    cleaning code

--- readSensor.py
+++ readSensor.py
@@ -13,17 +13,13 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 #Create the ADS object using the I2C bus
 ads = ADS.ADS1015(i2c)
-ads.gain = 8
 
 #Create single-ended input on channel 0 (Analog Pin 0)
-chan = AnalogIn(ads, ADS.P0)
-
-#Create differential input between channel 0 and 1 
-#chan = AnalogIn(ads, ADS.P0, ADS.P1)
+channelA0 = AnalogIn(ads, ADS.P0)
 
 print("MQ2 Warming Up!")
 time.sleep(5) # Give 5 seconds to warm up
 
 while True:
-	print(f"raw value: {chan.value}     voltage: {chan.voltage:.3f} volts")
+	print(f"raw value: {channelA0.value}     voltage: {channelA0.voltage:.3f} volts")
 	time.sleep(0.5) # allow delay between every read of about .5 sec
